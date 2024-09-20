eff13984a5adee2c6d8dfa4714534e6b7cdcac88
vnho203 <125419175+vnho203@users.noreply.github.com>
2024-09-20 08:01:11 +0700

    setup backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,29 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const app = express();
+
+app.get("/home",(req, res)=>{
+    return res.status(200).json({
+        message: "Hello World",
+        success: true
+    })
+});
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+})
